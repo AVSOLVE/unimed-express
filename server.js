b42a5d1b594d86f91b1bafd2da7d26b2534b8419
Alexandre Lima <alexandrelima.mail@gmail.com>
2024-12-01 22:37:30 -0300

    as

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.get('/', async (req, res) => {
     }
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
